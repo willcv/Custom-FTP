d4bb00f19a4847de84c01f1b6c86da29a5e4f242
rishilss <rishilsa@usc.edu>
2022-09-11 17:32:31 -0700

    Initial UDP Client Server setup

---
+++ Makefile
@@ -0,0 +1,13 @@
+# the compiler: gcc for C program, define as g++ for C++
+CC = g++
+
+all: server.out client.out
+
+server.out: server.cpp
+	@ $(CC) -o server server.cpp
+
+client.out: client.cpp
+	@ $(CC) -o client client.cpp
+
+clean:
+	@ $(RM) server client
---
+++ client.cpp
@@ -0,0 +1,105 @@
+// Client side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <string>
+
+#define CLIENT_PORT "25581"
+#define SERVER_PORT "25580"
+#define localhost "127.0.0.1"
+#define MAXLINE 1024
+
+using namespace std;
+
+// UDP Socket setup code from Beej’s Guide to Network Programming
+int SetupUDPSocket(const char *port)
+{
+    struct addrinfo hints;
+    struct addrinfo *res;
+    int sockfd;
+    int status;
+
+    // first, load up address structs with getaddrinfo():
+
+    memset(&hints, 0, sizeof hints); // make sure hints is empty
+    hints.ai_family = AF_INET;       // use IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // use datagram sockets
+
+    // error checking for getaddrinfo
+
+    if ((status = getaddrinfo(localhost, port, &hints, &res)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    // make a socket:
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    // error checking for socket creation
+
+    if (sockfd == -1)
+    {
+        perror("Central UDP: socket");
+        exit(1);
+    }
+
+    // bind it to the port and IP address we passed in to getaddrinfo():
+
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) == -1)
+    {
+        close(sockfd);
+        perror("Central UDP: bind");
+        exit(1);
+    }
+
+    freeaddrinfo(res); // all done with this structure
+    return sockfd;
+}
+
+// Driver code
+int main()
+{
+    int sockfd = SetupUDPSocket(CLIENT_PORT);
+
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    int status;
+    int numbytes;
+
+    // first, load up address structs with getaddrinfo():
+
+    memset(&hints, 0, sizeof hints); // make sure hints is empty
+    hints.ai_family = AF_INET;       // use IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // use datagram sockets
+
+    // error checking for getaddrinfo
+    // getaddrinfo used to get server address
+
+    if ((status = getaddrinfo(localhost, SERVER_PORT, &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    string hello = "Hello from client";
+
+    if ((numbytes = sendto(sockfd, hello.data(), hello.length(), 0, servinfo->ai_addr, servinfo->ai_addrlen)) == -1)
+    {
+        perror("Central: ServerP sendto num nodes");
+        exit(1);
+    }
+
+    printf("Hello message sent.\n");
+    close(sockfd);
+    return 0;
+}
