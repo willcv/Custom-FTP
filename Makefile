423dbe88ec23cdcae1f34527453122248a5acdce
Ubuntu <ubuntu@ip-10-0-3-114.us-west-2.compute.internal>
2022-09-15 21:40:29 +0000

    saving progress

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 # the compiler: gcc for C program, define as g++ for C++
-CC = g++
+CC = g++-7
 
 all: server.out client.out
 
 server.out: server.cpp
-	@ $(CC) -o server server.cpp
+	@ $(CC) -o server server.cpp -lpthread
 
 client.out: client.cpp
 	@ $(CC) -o client client.cpp -lpthread
--- server.cpp
+++ server.cpp
@@ -17,23 +17,29 @@
 #define SERVER_PORT "25580"
 #define MAIN_BUF_SIZE 65536*16384
 #define UDP_SIZE 65507
-#define UDP_DATA_SIZE UDP_SIZE - 4
-
+#define UDP_DATA_SIZE (UDP_SIZE - 4)
+#define MAX_SEQ_NUM (MAIN_BUF_SIZE - (MAIN_BUF_SIZE % UDP_DATA_SIZE))/UDP_DATA_SIZE
 #define localhost "127.0.0.1"
 
 using namespace std;
 
 int sockfd;
 int itr_done;
-unordered_set <int> neg_acks;
+unordered_set <int> remaining_packet_hash;
+pthread_mutex_t hash_mutex;
+pthread_mutex_t itr_done_mutex;
+pthread_mutex_t mainBuf_mutex;
 
+char * mainBuf;
 
 // UDP Socket setup code from Beej’s Guide to Network Programming
 int SetupUDPSocket(const char *port)
 {
     struct addrinfo hints;
     struct addrinfo *res;
-    int sockfd;
+    struct timeval read_timeout;
+    read_timeout.tv_sec = 1;
+    read_timeout.tv_usec = 0;
     int status;
 
     // first, load up address structs with getaddrinfo():
@@ -44,7 +50,7 @@ int SetupUDPSocket(const char *port)
 
     // error checking for getaddrinfo
 
-    if ((status = getaddrinfo(localhost, port, &hints, &res)) != 0)
+    if ((status = getaddrinfo(, port, &hints, &res)) != 0)
     {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
@@ -70,49 +76,153 @@ int SetupUDPSocket(const char *port)
         perror("Central UDP: bind");
         exit(1);
     }
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof(read_timeout));
 
     freeaddrinfo(res); // all done with this structure
     return sockfd;
 }
 
-void *send_thread(void*) {
+void *send_thread(void* serverinfo) {
 
+    printf("send thread started\n");
+    struct addrinfo *servinfo = (struct addrinfo *) serverinfo;
+	
+    int hash_size = MAX_SEQ_NUM;
+    int local_itr_done;
+    while (hash_size) {
+ 
+        pthread_mutex_lock(&itr_done_mutex);
+        local_itr_done = itr_done;
+        pthread_mutex_unlock(&itr_done_mutex);
+
+        if (local_itr_done) {
+            string ack_string = "";
+            pthread_mutex_lock(&hash_mutex);
+            for (const auto& elem: remaining_packet_hash) {
+                ack_string += to_string(elem) + " ";
+            }  
+	    pthread_mutex_unlock(&hash_mutex);
+
+            printf("Ack packet string %s\n", ack_string.c_str());
+            for (int i = 0; i < 5; i++) {
+
+                sendto(sockfd, ack_string.c_str(), ack_string.length(), 0, servinfo->ai_addr, servinfo->ai_addrlen);
+
+                printf("Sent Ack %d\n", i);
+            }
+            printf("Done sending ack packets\n");
+            break;
+        }
+        
+        // Update hash size
+        pthread_mutex_lock(&hash_mutex);
+        hash_size = remaining_packet_hash.size();
+        pthread_mutex_unlock(&hash_mutex);
+
+    }
+    
 
 }
 
-void *recv_thread(void*) {
+void *recv_thread(void* clientinfo) {
     
+    printf("recv thread started\n");
     int queue_empty = 1;
-    while(!queue_empty) {
+    int numbytes;
+    struct sockaddr_in cli_info = *(struct sockaddr_in *) clientinfo;
+    socklen_t addr_len = sizeof(cli_info);
+
+    char localBuf[UDP_SIZE]; 
+    int hash_size = MAX_SEQ_NUM;
+
+    while(hash_size) {
         
-       
+
+        numbytes = recvfrom(sockfd, (char *)localBuf, UDP_SIZE, 0, (struct sockaddr *) &cli_info, &addr_len);
+
+        if (numbytes < 0) {
+            
+            // Timeout has occured. Update hash size and restart loop.
+            pthread_mutex_lock(&hash_mutex);
+            hash_size = remaining_packet_hash.size();
+            pthread_mutex_unlock(&hash_mutex);
+            printf("timeout has occurred\n");
+            continue;
+        }
+
+
+        int seq_num = (localBuf[0] << 8) | localBuf[1] & 0xFF;
+        int packet_itr_done = (localBuf[2]) & 0x02;
+        int packet_full_done = (localBuf[2]) & 0x01;
+
+        printf("Received seq num: %d\n", seq_num);
+
+        if (packet_itr_done) {
+            // Done packet received. Set itr_done flag.
+            pthread_mutex_lock(&itr_done_mutex);
+            itr_done = 1;
+            pthread_mutex_unlock(&itr_done_mutex);
+        }
+        else {
+            // Data packet received. Store data in buffer
+            pthread_mutex_lock(&mainBuf_mutex);
+            memcpy(&mainBuf[seq_num*UDP_DATA_SIZE], &localBuf[4], UDP_DATA_SIZE);
+            pthread_mutex_unlock(&mainBuf_mutex);
+
+            // Update hash
+            pthread_mutex_lock(&hash_mutex);
+            remaining_packet_hash.erase(seq_num);
+            hash_size = remaining_packet_hash.size();
+            pthread_mutex_unlock(&hash_mutex);
+        }
 
     }
+
+    
 }
 
 // Driver code
 int main()
 {
+
     sockfd = SetupUDPSocket(SERVER_PORT);
     struct sockaddr_in cliaddr;
     socklen_t addr_len = sizeof(cliaddr);
     int numbytes;
 
-    char buffer[MAXLINE];
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    int status;
 
-    while (1)
+    // first, load up address structs with getaddrinfo():
+
+    memset(&hints, 0, sizeof hints); // make sure hints is empty
+    hints.ai_family = AF_INET;       // use IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // use datagram sockets
+
+    // error checking for getaddrinfo
+    // getaddrinfo used to get server address
+
+    if ((status = getaddrinfo(localhost, SERVER_PORT, &hints, &servinfo)) != 0)
     {
-        if ((numbytes = recvfrom(sockfd, (char *)buffer, MAXLINE, 0, (struct sockaddr *)&cliaddr, &addr_len)) == -1)
-        {
-            perror("ServerP: Central recvfrom scores list");
-            exit(1);
-        }
-
-        buffer[numbytes] = '\0';
-        printf("Client : %s\n", buffer);
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
     }
 
+
+
+    for (int i = 0; i < MAX_SEQ_NUM; i++ ) {
+        remaining_packet_hash.insert(i);
+    }
+
+    mainBuf = new char[MAIN_BUF_SIZE];
+    pthread_t tid[2];
+    pthread_create(&tid[0], NULL, recv_thread, (void*) &cliaddr);
+    pthread_create(&tid[1], NULL, send_thread, (void*) servinfo);
+    
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
     close(sockfd);
 
     return 0;
-:}
+}
