0cc0803dfd6e06870d7853750cd88f2440a8f0f8
rishilss <rishilsa@usc.edu>
2022-09-16 19:07:14 -0700

    Changed Client ACK receive port

--- common.h
+++ common.h
@@ -6,12 +6,12 @@
 #define UDP_DATA_SIZE UDP_SIZE - 5
 
 #define SERVER_IP                                "10.0.2.66"
-#define SERVER_MAIN_PORT                         "25580"
+#define SERVER_MAIN_PORT                         "25581"
 static const char* const SERVER_THREAD_PORTS[] = {"25581", "25582", "25583", "25584", "25585"};
 
 
 #define CLIENT_IP                                "10.0.1.115"
-#define CLIENT_MAIN_PORT                         "26580"
+#define CLIENT_MAIN_PORT                         "25580"
 static const char* const CLIENT_THREAD_PORTS[] = {"26581", "26582", "26583", "26584", "26585"};
 
 #endif
